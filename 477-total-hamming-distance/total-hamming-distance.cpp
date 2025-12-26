b064fed712056eba5f58154d2c35b1b75be91f72
Aman Katiyar <amanktyr@gmail.com>
2025-12-26 18:00:28 +0530

    Time: 17 ms (24.61%) | Memory: 22.9 MB (13.25%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& arr) 
-    {
+    {    //overall  complexit  O(n) not O(n^2)
        int n=arr.size(),count=0,i=32;
-       while(i--)              
+       while(i--)        // constant time becz only 32 times will run this loop         
        { int ones=0,zeroes=0;
          for(int j=0;j<n;j++)
          {
