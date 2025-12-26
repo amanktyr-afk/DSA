72fd214ce794b9ecab329b2f5ae9c236ca475542
Aman Katiyar <amanktyr@gmail.com>
2025-12-26 11:26:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (66.88%) - LeetSync

--- 461-hamming-distance/hamming-distance.cpp
+++ 461-hamming-distance/hamming-distance.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) 
-    {
-       int count=0;
-       int i=32;
+    {   int ans=0,i=32,count=0;
+        ans= x^y;
        while(i--)
        {
-        if((x&1)!=(y&1))  // & has lower priority then !=
-        count++;
-        x>>=1;y>>=1;
+         count+=(ans&1);
+         ans>>=1;
        }
-       return count;;
+       return count;
     }
 };
