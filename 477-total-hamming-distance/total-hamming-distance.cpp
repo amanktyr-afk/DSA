a5b6546c849ffa05ccb4fb4c675abc154f280ffc
Aman Katiyar <amanktyr@gmail.com>
2025-12-26 17:53:45 +0530

    Time: 21 ms (15.77%) | Memory: 22.7 MB (77.50%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -3,17 +3,17 @@ public:
     int totalHammingDistance(vector<int>& arr) 
     {
        int n=arr.size(),count=0,i=32;
-       while(i--)
+       while(i--)              
        { int ones=0,zeroes=0;
          for(int j=0;j<n;j++)
          {
-          if(arr[j]&1)
-          ones++;
+          if(arr[j]&1)    // checking last bit of each ele is set or not
+          ones++;         
           else
           zeroes++;
           arr[j]>>=1;
           }
-          count=count+(ones*zeroes);
+          count=count+(ones*zeroes);      //ones*zeroes   will give no of hamming distance by each  lsb 
        }
        return count;
     }
