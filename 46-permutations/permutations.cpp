23fc4fa2ae62a18c8f699a7e53f0ef57a5dfc916
Aman Katiyar <amanktyr@gmail.com>
2025-12-27 23:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (69.51%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void permutation(vector<int>&nums,vector<vector<int>>&ans,vector<int>&visited,vector<int>&temp,int index)
+    void permutation(vector<int>&nums,vector<vector<int>>&ans,vector<int>&visited,vector<int>&temp)
     {  
-         if(index==visited.size())
+         if(temp.size()==visited.size())
        {
          ans.push_back(temp);
          return ;
@@ -13,8 +13,8 @@ public:
          if(visited[i]==0)
          {
             visited[i]=1;
-            temp.push_back(nums[i]);
-            permutation(nums,ans,visited,temp,index+1);
+             temp.push_back(nums[i]);
+            permutation(nums,ans,visited,temp);
             visited[i]=0;
             temp.pop_back();
          }   
@@ -24,8 +24,8 @@ public:
     {   int n=nums.size(),index=0;
        vector<vector<int>> ans;
        vector<int> visited(n,0);
-       vector<int> temp;
-       permutation(nums,ans,visited,temp,index); 
+       vector<int> temp;              // if we'll define temp size it;s error
+       permutation(nums,ans,visited,temp); 
        return ans; 
     }
 };
