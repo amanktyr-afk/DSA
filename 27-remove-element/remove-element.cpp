b11912db5ab2cc70d424c1e07bdea25519bbc0f8
Aman Katiyar <amanktyr@gmail.com>
2026-01-04 14:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (46.95%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+//  O(n) approach
     int removeElement(vector<int>& nums, int val)
-     {
+     {   
         int n=nums.size();
-        int first=0,sec=1,count=0;
-        while(sec<n)
+        int first=0,last=n-1,count=0,i=0;
+        while(first<last)
         {
-            if(nums[first]==val)
-            {
-                while(nums[sec]==val&&sec<n-1)
-                sec++;
-                swap(nums[first],nums[sec]);
-            }
-                    first++;sec++;
+          if(n==0)
+          return 0;
+          if(nums[first]!=val)
+          first++;
+          else if(nums[first]==val&&nums[last]==val)
+          last--;
+          else 
+          {
+             swap(nums[first],nums[last]);
+             first++;last--;  
+          }   
         }
         for(int i=0;i<n;i++)
-        {
-            if(nums[i]!=val)
-            count++;
-        }
-        return count;
+       {
+        if(nums[i]!=val)
+        count++;
+       }
+        return count;  
      }
 };
