1dabd46d023ae64f374eb37c223e0ad18e84d93d
Aman Katiyar <amanktyr@gmail.com>
2026-01-29 16:56:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
@@ -14,18 +14,9 @@ class Node {
 class Solution {
   public:
     Node* sortedMerge(Node* head1, Node* head2) 
-    {     Node *temp=NULL;
-           if(head1->data>=head2->data)
-            {
-                temp=new Node(head2->data);
-                head2=head2->next;
-            }
-            else
-            {
-                temp=new Node(head1->data);
-                head1=head1->next;
-            }
-           Node* head=temp;
+    {    
+        Node *temp=new Node(5);// adding a dummy node for writting short code  
+           Node* head=temp;  //by 1 if-else we have stored the address that is going to be returned   
             while(head1&&head2)
             {
               if(head1->data>=head2->data)
@@ -41,12 +32,14 @@ class Solution {
                  temp=temp->next;
                }
             }
+            // inserting remaing L1 list
             while(head1)
             {
                 temp->next=new Node(head1->data);
                 head1=head1->next;
                  temp=temp->next;
             }
+             // inserting remaing L2 list
             while(head2)
             {
                 temp->next=new Node(head2->data);
@@ -54,6 +47,6 @@ class Solution {
                  temp=temp->next;
             }
             temp->next=NULL;
-            return head;
+            return head->next;// Dummy->next that is real list
     }
 };
