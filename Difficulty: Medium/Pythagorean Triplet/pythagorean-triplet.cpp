a8d7b5ad2082d9cac7fd75e67ebf763dad43e3d9
Aman Katiyar <amanktyr@gmail.com>
2026-02-16 00:14:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    bool pythagoreanTriplet(vector<int>& arr) 
+    {
+       int n=arr.size();
+       int maxele=0;
+       for(int i=0;i<n;i++)
+        maxele=max(maxele,arr[i]);
+      vector<bool>visited(maxele+1,0);
+      for(int i=0;i<n;i++)
+        visited[arr[i]]=1;  
+      for(int i=1;i<=maxele;i++)
+      {
+          if(visited[i]==0)
+           continue;
+           for(int j=1;j<=maxele;j++)
+           {
+             if(visited[j]==0)
+             continue;
+             int c=sqrt(i*i+j*j);
+             if(c*c!=i*i+j*j||c>maxele)
+             continue;
+             if(visited[c]==1)
+             return 1;
+           }
+      }
+      return 0;
+    }
+};
