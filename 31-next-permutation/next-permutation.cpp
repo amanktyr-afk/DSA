0bb2486db7cd33fd67ad949ef46842161d5cc0ad
Aman Katiyar <amanktyr@gmail.com>
2026-02-24 03:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (32.70%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) 
+    {  
+       int n=nums.size();
+       int i=n-1;
+       while(i>0)
+       {  
+          
+          if(nums[i-1]<nums[i])
+          { 
+            int minele=INT_MAX,index=-1;
+            for(int j=i;j<n;j++)
+            {
+              if(nums[i-1]<nums[j]&&nums[j]<minele)
+              {
+                minele=nums[j];
+                index=j;
+              }
+            }
+            swap(nums[i-1],nums[index]);
+            sort(nums.begin()+i,nums.end());
+            break;
+          }
+          i--;
+       } 
+//edge case when alllexo permutation end comes at permutation
+       if(i==0)
+          {
+            sort(nums.begin(),nums.end());
+          }
+    }
+};
