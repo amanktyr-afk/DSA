3f3290eac37265aaee378f8a46bf9b1db1e687a3
Aman Katiyar <amanktyr@gmail.com>
2026-03-19 15:45:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Generate Grey Code Sequences/generate-grey-code-sequences.cpp
+++ Difficulty: Easy/Generate Grey Code Sequences/generate-grey-code-sequences.cpp
@@ -2,30 +2,23 @@
 You are required to complete below method. */
 class Solution {
   public:
-    string bin_to_gray(int i,int N)
-    {  
-        string res="";
-        while(i>0)
-        {
-            res=char((i%2)+'0')+res;
-            i/=2;
-        }
-        res=string(N-res.length(),'0')+res; // get binary
-        string gray=res;
-        for(int j=1;j<N;j++)
-        {
-            gray[j]=((res[j-1]-'0')^(res[j]-'0'))+'0';
-        }
-        return gray;
-    }
     vector<string> generateCode(int N)
     {
       vector<string>ans;
-      for(int i=0;i<pow(2,N);i++) // or 1<<N = 2^N times (left shift) 
+      for(int i=0;i^(1<<N);i++)
       {
-          ans.push_back(bin_to_gray(i,N));
+         int gray=i^(i>>1);
+         string s="";
+         for(int j=0;j<N;j++)
+         {
+            if(gray & (1<<j))
+            s='1'+s;
+            else
+            s='0'+s;
+         }
+         ans.push_back(s);
       }
       return ans;
-      
     }
 };
+
