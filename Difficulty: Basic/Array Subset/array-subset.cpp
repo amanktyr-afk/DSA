96578cc06f25c1ab9cbc6ccf276a6bdedc681a6a
Aman Katiyar <amanktyr@gmail.com>
2026-03-31 01:36:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    // Function to check if b is a subset of a
+    bool isSubset(vector<int> &a, vector<int> &b) 
+    {
+       unordered_map<int,int>freq;
+       for(int i=0;i<a.size();i++)
+       {
+           freq[a[i]]++;
+       }
+       for(int i=0;i<b.size();i++)
+       {
+         if(freq[b[i]]==0)
+           return 0;
+          else
+          freq[b[i]]--;
+       }
+       return 1;
+    }
+};
