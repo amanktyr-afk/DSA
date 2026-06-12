2ed00a5d11529dd628b44b24331ff59e9da5161e
Aman Katiyar <amanktyr@gmail.com>
2026-06-12 15:56:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
@@ -1,22 +1,20 @@
 class Solution {
   public:
-    int missingNumber(vector<int> &arr)
+    int missingNumber(vector<int> &arr) 
     {
-      int n = arr.size();
-      for(int i = 0; i < n; i++)
+      int n=arr.size();
+      for(int i=0;i<n;i++)
       {
-         while(arr[i] >= 1 && arr[i] <= n &&
-              arr[i] != arr[arr[i] - 1])
-      {
-            swap(arr[i], arr[arr[i] - 1]);
+         while(arr[i]>0 && arr[i]<=n&& arr[i]!=arr[arr[i]-1])
+         {
+            swap(arr[i],arr[arr[i]-1]);
+         }
       }
+      for(int i=0;i<n;i++)
+      {
+          if(arr[i]!=i+1)
+          return i+1;
+      }
+      return n+1;;
     }
-
-    for(int i = 0; i < n; i++) {
-        if(arr[i] != i + 1)
-            return i + 1;
-    }
-
-    return n + 1;
-}
 };
