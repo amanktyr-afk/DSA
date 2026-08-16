f21b54c6cb0d5d23d11ed731afb1c45bafefc93b
Aman Katiyar <amanktyr@gmail.com>
2026-08-16 21:07:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked List Delete at Position/linked-list-delete-at-position.cpp
@@ -0,0 +1,28 @@
+
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* deleteNode(Node*cur, int x)
+    {
+      if(x==1)
+      {
+          Node*temp=cur->next;
+          delete cur;
+          return temp;
+      }
+      cur->next=deleteNode(cur->next,x-1);
+      return cur;
+
+    }
+};
