ee4482a4e35c30cf3c374c503195938a1fce208a
Aman Katiyar <amanktyr@gmail.com>
2026-08-16 16:20:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.cpp
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.cpp
@@ -1,3 +1,4 @@
+
 /*
 struct Node {
   int data;
@@ -11,31 +12,31 @@ struct Node {
 class Solution {
   public:
     // Function to remove duplicates from sorted linked list.
-    // soln with extra space
+    // soln with NO extra space
     Node* removeDuplicates(Node* head)
     {
-        vector<int>v;
-        Node* curr=head;
-        v.push_back(curr->data);// 1st ele pushed
-        curr=curr->next;   // points sec node 
-        while(curr)     
-        {  
-           if(v[v.size()-1]!=curr->data)
-            v.push_back(curr->data);
-            curr=curr->next;
-        }
-        curr=head;
-        Node*prev=NULL;
-        int index=0;
-        while(index<v.size())
-        {   
-            curr->data=v[index];
-            index++;
-            prev=curr;
-            curr=curr->next;
-        }
-        prev->next=NULL;
-        return head;
+
+      Node*curr=head->next;
+      Node*prev=head;
+      while(curr)
+      {   
+          if(!head)   // if #nodes can be 0, but here no need
+          return head;
+
+          if(curr->data==prev->data)
+          {
+              prev->next=curr->next;
+              delete curr;
+              curr=prev->next;
+          }
+          else
+          {
+              prev=curr;
+              curr=curr->next;
+          }
+
+      }
+      return head;
     }
 
 };
