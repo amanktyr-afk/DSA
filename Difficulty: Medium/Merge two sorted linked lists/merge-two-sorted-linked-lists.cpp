04714d156c82ad8b886ef377ca61e97562c95b94
Aman Katiyar <amanktyr@gmail.com>
2026-08-24 13:01:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
@@ -13,40 +13,36 @@ class Node {
 
 class Solution {
   public:
+  // solution with no extra space(node creation)
     Node* sortedMerge(Node* head1, Node* head2) 
     {    
-        Node *temp=new Node(5);// adding a dummy node for writting short code  
-           Node* head=temp;  //by 1 if-else we have stored the address that is going to be returned   
+        Node *head=new Node(5);// adding a dummy node for writting short code  
+           Node* tail=head;  //by 1 if-else we have stored the address that is going to be returned   
             while(head1&&head2)
             {
               if(head1->data>=head2->data)
               {
-                temp->next=new Node(head2->data);
+                tail->next=head2;
                 head2=head2->next;
-                temp=temp->next;
+                tail=tail->next;
                }
               else
               {
-                temp->next=new Node(head1->data);
+                tail->next=head1;
                 head1=head1->next;
-                 temp=temp->next;
+                tail=tail->next;
                }
             }
-            // inserting remaing L1 list
-            while(head1)
-            {
-                temp->next=new Node(head1->data);
-                head1=head1->next;
-                 temp=temp->next;
-            }
-             // inserting remaing L2 list
-            while(head2)
-            {
-                temp->next=new Node(head2->data);
-                head2=head2->next;
-                 temp=temp->next;
-            }
-            temp->next=NULL;
-            return head->next;// Dummy->next that is real list
+            // linking remaing L1 list
+            if(head1)
+             tail->next=head1;
+             // linking remaing L2 list
+            if(head2)
+            tail->next=head2;
+        
+            tail=head;
+            head= head->next;// Dummy->next that is real list
+             delete tail;    // removing that dummy node
+             return head;
     }
 };
